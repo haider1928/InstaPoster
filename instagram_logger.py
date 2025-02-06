5b1b1de268eb50273a6c6df3516e70b84da517e5
haider1928 <153923816+haider1928@users.noreply.github.com>
2025-02-06 09:35:02 +0500

    Update instagram_logger.py

--- instagram_logger.py
+++ instagram_logger.py
@@ -7,3 +7,14 @@ def login(username, password):
         bot = instagrapi.Client()
         status = bot.login(username=username, password=password)
         return bot
+
+def login_by_session_id(session_id):
+    if session_id:
+        bot = instagrapi.Client()
+        status = bot.login_by_sessionid(sessionid=session_id)
+        return bot
+    
+
+def logout(bot):
+    status = bot.logout()
+    return status
