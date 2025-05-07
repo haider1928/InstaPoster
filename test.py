4b1aacc2e80f0f3b9d8a4387700bc0689ce8e93b
vapours <153923816+haider1928@users.noreply.github.com>
2025-05-07 12:46:06 +0500

    jkj

---
+++ test.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+# Wait for 10 minutes (600 seconds)
+time.sleep(420)
+
+# Type the message
+pyautogui.write("Hey Haiderina im eating dinner, just wait a few minutes, this is an automated message")
+
+# Press Enter
+pyautogui.press("enter")
